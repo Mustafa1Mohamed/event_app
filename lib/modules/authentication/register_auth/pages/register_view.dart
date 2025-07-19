0589d9ee23f5b0d954326f424b130e97fb4b3ae0
Mustafa <mm0118250@gmail.com>
2025-07-19 16:41:55 +0300

    give padding to the screen

--- lib/modules/authentication/register_auth/pages/register_view.dart
+++ lib/modules/authentication/register_auth/pages/register_view.dart
@@ -24,120 +24,123 @@ class _RegisterViewState extends State<RegisterView> {
         backgroundColor: Colors.white,
       ),
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Image.asset(AppAssets.logo, width: 159),
-            const SizedBox(height: 24),
-            const CustomTextFormField(
-              hintText: 'Name',
-              iconPath: AppAssets.personIcon,
-            ),
-            const SizedBox(height: 16),
-            const CustomTextFormField(
-              hintText: 'Email',
-              iconPath: AppAssets.mailIcon,
-            ),
-            const SizedBox(height: 16),
-            const CustomTextFormField(
-              hintText: 'Password',
-              isPassword: true,
-              iconPath: AppAssets.lockIcon,
-            ),
-            const SizedBox(height: 16),
-            const CustomTextFormField(
-              hintText: 'Re Password',
-              isPassword: true,
-              iconPath: AppAssets.lockIcon,
-            ),
-            const SizedBox(height: 16),
-            CustomButton(text: 'Create Account'),
-            const SizedBox(height: 16),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  'Already have an account? ',
-                  style: TextStyle(fontWeight: FontWeight.w500, fontSize: 16),
-                ),
-                InkWell(
-                  onTap: () {
-                    Navigator.pop(context);
-                  },
-                  child: Text(
-                    'Login',
-                    style: TextStyle(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            children: [
+              Image.asset(AppAssets.logo, width: 159),
+              const SizedBox(height: 24),
+              const CustomTextFormField(
+                hintText: 'Name',
+                iconPath: AppAssets.personIcon,
+              ),
+              const SizedBox(height: 16),
+              const CustomTextFormField(
+                hintText: 'Email',
+                iconPath: AppAssets.mailIcon,
+              ),
+              const SizedBox(height: 16),
+              const CustomTextFormField(
+                hintText: 'Password',
+                isPassword: true,
+                iconPath: AppAssets.lockIcon,
+              ),
+              const SizedBox(height: 16),
+              const CustomTextFormField(
+                hintText: 'Re Password',
+                isPassword: true,
+                iconPath: AppAssets.lockIcon,
+              ),
+              const SizedBox(height: 16),
+              CustomButton(text: 'Create Account'),
+              const SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Already have an account? ',
+                    style: TextStyle(fontWeight: FontWeight.w500, fontSize: 16),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text(
+                      'Login',
+                      style: TextStyle(
+                        color: ColorPalette.primaryColor,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 24),
+              Center(
+                child: Container(
+                  width: 65,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(16),
+                    border: Border.all(
                       color: ColorPalette.primaryColor,
-                      fontSize: 16,
-                      fontWeight: FontWeight.w500,
+                      width: 1,
                     ),
                   ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 24),
-            Center(
-              child: Container(
-                width: 65,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(16),
-                  border: Border.all(
-                    color: ColorPalette.primaryColor,
-                    width: 1,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      InkWell(
+                        onTap: () {
+                          setState(() {
+                            isPressed = !isPressed;
+                          });
+                        },
+                        child: Container(
+                          width: 27,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(16),
+                            border: Border.all(
+                              color: isPressed
+                                  ? ColorPalette.primaryColor
+                                  : Colors.transparent,
+                              width: isPressed ? 4 : 0,
+                            ),
+                          ),
+                          child: Image.asset(AppAssets.americaFlag, width: 26),
+                        ),
+                      ),
+                      const SizedBox(width: 8),
+                      InkWell(
+                        onTap: () {
+                          setState(() {
+                            isPressed = !isPressed;
+                          });
+                        },
+                        child: Container(
+                          width: 27,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(16),
+                            border: Border.all(
+                              color: isPressed
+                                  ? Colors.transparent
+                                  : ColorPalette.primaryColor,
+                              width: isPressed ? 0 : 4,
+                            ),
+                          ),
+                          child: Image.asset(AppAssets.EGIcon, width: 26),
+                        ),
+                      ),
+                    ],
                   ),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    InkWell(
-                      onTap: () {
-                        setState(() {
-                          isPressed = !isPressed;
-                        });
-                      },
-                      child: Container(
-                        width: 27,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(16),
-                          border: Border.all(
-                            color: isPressed
-                                ? ColorPalette.primaryColor
-                                : Colors.transparent,
-                            width: isPressed ? 4 : 0,
-                          ),
-                        ),
-                        child: Image.asset(AppAssets.americaFlag, width: 26),
-                      ),
-                    ),
-                    const SizedBox(width: 8),
-                    InkWell(
-                      onTap: () {
-                        setState(() {
-                          isPressed = !isPressed;
-                        });
-                      },
-                      child: Container(
-                        width: 27,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(16),
-                          border: Border.all(
-                            color: isPressed
-                                ? Colors.transparent
-                                : ColorPalette.primaryColor,
-                            width: isPressed ? 0 : 4,
-                          ),
-                        ),
-                        child: Image.asset(AppAssets.EGIcon, width: 26),
-                      ),
-                    ),
-                  ],
                 ),
               ),
-            ),
-            const SizedBox(height: 24),
-          ],
+              const SizedBox(height: 24),
+            ],
+          ),
         ),
       ),
     );
